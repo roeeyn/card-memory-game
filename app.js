182d5b7f9f385dd09df40ffc6726f2ba0e85082d
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2021-02-01 20:52:52 -0600

    Added solution

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
-// Helped you a little by adding some boilerplate code.
-// Feel free to modify it, if you believe is necessary.
-const grid = document.querySelector(".grid");
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
 
-const cardsArray = [
-  { name: "cheeseburger", img: "images/cheeseburger.png" },
-  { name: "fries", img: "images/fries.png" },
-  { name: "hotdog", img: "images/hotdog.png" },
-  { name: "ice-cream", img: "images/ice-cream.png" },
-  { name: "milkshake", img: "images/milkshake.png" },
-  { name: "pizza", img: "images/pizza.png" },
-]
-  .reduce((acc, element) => [...acc, element, element], [])
-  .sort(() => 0.5 - Math.random());
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let wonCards = [];
 
-// fill cards grid
-cardsArray.forEach(({ img, name }, idx) => {
-  const card = document.createElement("img");
-  card.setAttribute("src", "images/blank.png");
-  grid.appendChild(card);
+  const cardsArray = [
+    { name: "cheeseburger", img: "images/cheeseburger.png" },
+    { name: "fries", img: "images/fries.png" },
+    { name: "hotdog", img: "images/hotdog.png" },
+    { name: "ice-cream", img: "images/ice-cream.png" },
+    { name: "milkshake", img: "images/milkshake.png" },
+    { name: "pizza", img: "images/pizza.png" },
+  ]
+    .reduce((acc, element) => [...acc, element, element], [])
+    .sort(() => 0.5 - Math.random());
+
+  const checkMatch = () => {
+    const allCards = document.querySelectorAll("img");
+    const firstCard = cardsChosen[0];
+    const secondCard = cardsChosen[1];
+    if (firstCard === secondCard) {
+      allCards[cardsChosenId[0]].classList.add("invisible");
+      allCards[cardsChosenId[1]].classList.add("invisible");
+    } else {
+      allCards[cardsChosenId[0]].setAttribute("src", `images/blank.png`);
+      allCards[cardsChosenId[1]].setAttribute("src", `images/blank.png`);
+    }
+
+    // check if its a match
+    if (firstCard === secondCard) {
+      wonCards = [...wonCards, firstCard];
+      resultDisplay.textContent = wonCards.length;
+    }
+
+    // check for finished match
+    if (wonCards.length === allCards.length / 2) alert("ya ganaste puñetas");
+
+    // reset arrays
+    cardsChosen = [];
+    cardsChosenId = [];
+  };
+
+  const flipCard = (img, name, { target }) => {
+    if (target.classList.contains("invisible")) return;
+    const cardId = target.getAttribute("data-id");
+    target.setAttribute("src", img);
+    cardsChosen = [...cardsChosen, name];
+    cardsChosenId = [...cardsChosenId, cardId];
+    if (cardsChosen.length === 2) setTimeout(checkMatch, 500);
+  };
+
+  // fill cards grid
+  cardsArray.forEach(({ img, name }, idx) => {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/blank.png");
+    card.setAttribute("data-id", idx);
+    card.addEventListener("click", (event) => flipCard(img, name, event));
+    grid.appendChild(card);
+  });
 });
-
-// Your amazing code here 🚀
-// PRO Tip: For making a card disappear, add the class \
-// "invisible" to it. 🔥
